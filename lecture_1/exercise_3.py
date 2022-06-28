07063420b85834c7498d3f096bce063e8d61ed8b
MinnullinIlshat <92851163+MinnullinIlshat@users.noreply.github.com>
2022-06-28 05:47:55 +0300

    Update exercise_3.py

--- lecture_1/exercise_3.py
+++ lecture_1/exercise_3.py
@@ -1,12 +1,6 @@
-from math import factorial
-
 def zeros(n):
-    """сравниваем длину строки с нулями в конце, и без них"""
-    s = str(factorial(n))
-    return len(s) - len(s.strip('0'))
-
-if __name__ == '__main__':
-    assert zeros(0) == 0
-    assert zeros(6) == 1
-    assert zeros(30) == 7
-
+    res = 0
+    while n > 0:
+        n //= 5
+        res += n
+    return res
