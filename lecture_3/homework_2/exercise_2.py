2de98c0583e197adbdacf1c4723271af9387a3a0
Ilshat Minnullin <minnullinilshat@gmail.com>
2022-07-11 10:57:03 +0300

    completed

--- lecture_3/homework_2/exercise_2.py
+++ lecture_3/homework_2/exercise_2.py
@@ -9,13 +9,11 @@ class Movie:
     dates: List[Tuple[datetime, datetime]]
 
     def schedule(self) -> Generator[datetime, None, None]:
-        all_dates = []
         for date in self.dates:
             current_date = date[0]
             while current_date <= date[1]:
-                all_dates.append(current_date)
+                yield current_date
                 current_date += timedelta(days=1)
-        return (date for date in all_dates)
 
 if __name__ == '__main__':
     m = Movie('sw', [
